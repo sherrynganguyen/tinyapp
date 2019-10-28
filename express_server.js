3676e3c7f6d2e107d06bdbf50b15dda60ae09a98
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-10-28 22:17:25 +0000

    Update with template

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const reload = require("reload");
 
 app.set("view engine", "ejs");
 
@@ -9,12 +10,17 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"  
 };
 
+app.get("/urls", (req, res) => {
+  let templateVars = {url: urlDatabase};
+  res.render('urls_index', templateVars);
+});
 app.get("/", (req, res) => {
   res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  let templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
 });
 
 app.listen(PORT, () => {

0c839617a72c72ee9983c9889b519a5e0f68697b
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-10-29 02:47:35 +0000

    Update

--- express_server.js
+++ express_server.js
@@ -1,3 +1,11 @@
+// |x| After we generate our new shortURL, we add it to our database.
+// |x| Our server then responds with a redirect to /urls/:shortURL.
+// | | Our browser then makes a GET request to /urls/:shortURL.
+// | | Our server looks up the longURL from the database, sends the shortURL and longURL to the urls_show template, generates the HTML, and then sends this HTML back to the browser.
+// | | The browser then renders this HTML.
+
+
+
 const express = require("express");
 const app = express();
 const PORT = 8080;
@@ -6,20 +14,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"  
+let urlDatabase = {
+  "b2xVn2": "https://www.lighthouselabs.ca",
+  "9sm5xK": "https://www.google.com"  
 };
 
+function generateRandomString() {
+  const randomData = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  let randomString = "";
+  for (let i = 0; i < 6; i++) {
+    randomString += randomData.charAt(Math.floor(Math.random()*(randomData.length)));
+  }
+  return randomString;
+};
 app.get("/", (req, res) => {
   res.json(urlDatabase);
 });
-
-app.get("/hello", (req, res) => {
-  let templateVars = { greeting: 'Hello World!' };
-  res.render("hello_world", templateVars);
-});
-
 
 app.get("/urls", (req, res) => {
   let templateVars = {urlDatabase: urlDatabase};
@@ -31,15 +41,19 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  let t = `${generateRandomString()}`;
+  urlDatabase[t] = req.body.longURL;
+  res.redirect(`/urls/${t}`);
 })
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  res.render("urls_show", templateVars);
+  if (urlDatabase[req.params.shortURL.charAt('http') > 0]) {
+    res.redirect(urlDatabase[req.params.shortURL]);
+  } else {
+    res.redirect("http://" + urlDatabase[req.params.shortURL]);    
+  }
+
 });
-
-
 
 app.listen(PORT, () => {
   console.log("Example app listening on port ${PORT}!");

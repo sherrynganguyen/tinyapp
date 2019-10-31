9c75b05fedfc86e1e7ecf9203cb38d4bbc429392
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-10-31 20:47:30 +0000

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -10,9 +10,9 @@ const generateRandomString = function() {
 const checkUserByEmail = function(email, database) {
   for (let user in database) {
     if (email === database[user].email) {
-      return false;
+      return database[user].id;
     }
-    return true;
+    return undefined;
   }
 };
 
@@ -26,21 +26,8 @@ const findUserByEmail = function(email, database) {
   return verifiedID;
 };
 
-console.log(typeof findUserByEmail('123@example.com', {
-  "e2yh32gdTnuPchcElCmV+S6ZIKQ9x7mkxq0YMA==": {
-    id: "e2yh32gdTnuPchcElCmV+S6ZIKQ9x7mkxq0YMA==",
-    email: "user@example.com",
-    password: "$2b$10$f9ETSYlgdTFV53vpYMWjG.epNDqxBEIJWvKPfTZVowKtTp0wJYYX6"
-  },
-  "Sw5Rg2O2Sa5Vimm8HTcwZ/QpNKbXnutYJxuXmKc=": {
-    id: "Sw5Rg2O2Sa5Vimm8HTcwZ/QpNKbXnutYJxuXmKc=",
-    email: "user2@example.com",
-    password: "$2b$10$yrRsbjMU6.9z1WhUaqGh0OUtx51oC9NdZj7fIXaGIOAJ/DuLIoozi"
-  }
-}));
-
 const findUserURL = function(userID, database) {
-  let userURLList = [];
+  let userURLList = {};
   for (let shortURL in database) {
     if (userID === database[shortURL].userID) {
       userURLList[shortURL] = database[shortURL].longURL;
